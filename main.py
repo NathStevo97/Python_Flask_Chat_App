e33153d26ecb8846584e8db7d6afc0d9fd523273
NathStevo97 <natstephenson15@gmail.com>
2019-08-15 09:14:33 +0100

    test

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     socketio.emit('my response', json, callback=messageReceived)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, 'localhost', port=5000, debug=True)
 
